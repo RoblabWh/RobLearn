2325fdd266473d1c99c493ffe2adf8283442039f
Robin Marchel <hallo@rmarchel.de>
2018-07-03 00:46:46 +0200

    Implement Threading

--- NeuronalNetwork/OneStepLearningAgent.py
+++ NeuronalNetwork/OneStepLearningAgent.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from random import random, randrange
 
@@ -25,10 +26,13 @@ target_update_timestep = 2000
 gamma = 0.99
 # Number of timesteps to anneal epsilon
 anneal_epsilon_timesteps = 400000
+CLUSTER_SIZE = 36
+WORKER_THREADS = 2
 
+INITIAL_EPSILON = 1.0
 final_epsilon = 0.02
 
-visualize = True
+visualize = False
 
 
 class OneStepLearningAgent(object):
@@ -57,7 +61,7 @@ class OneStepLearningAgent(object):
         action_q_values = tf.reduce_sum(tf.multiply(self.model, self.updater_a), reduction_indices=1)
         cost = tflearn.mean_square(action_q_values, self.updater_y)
         # optimizer = tf.train.RMSPropOptimizer(0.001)
-        optimizer = tf.train.AdamOptimizer(0.002)
+        optimizer = tf.train.AdamOptimizer(0.002) # @TODO Learning rate
         grad_update = optimizer.minimize(cost, var_list=self.network_params)
         return grad_update
 
@@ -79,75 +83,103 @@ class OneStepLearningAgent(object):
     def train(self, session, env):
         session.run(tf.initialize_all_variables())
 
-        num_episode = 0
+        global_episode = 0
         global_step = 0
+
+        global global_episode, global_step
 
         initial_epsilon = 1.0
         epsilon = initial_epsilon
 
-        while num_episode <= MAX_EPISODES:
-            reset_env(env)
-            state, _, _, _ = env.step(0, 0)
-            state = np.reshape(state, [1, self.state_size, 1]) # @TODO Auslagern
+        graph_ops = {
+            'network': {
+                'q_values': self.model,
+                'input': self.input
+            },
+            'target_network' : {
+                'q_values': self.target_model,
+                'input': self.target_input
+            }
+        }
 
-            num_episode += 1
+        update_ops = {
+            'reset_target_network': self.reset_target_net,
+            'minimize': self.update,
+            'a': self.updater_a,
+            'y': self.updater_y
+        }
 
-            episode_step = 0
-            episode_reward = 0
-            while True:
-                q_values = self.model.eval(session=session, feed_dict={self.input: state})
+        agents = [WorkerAgent(i, graph_ops, update_ops, None, session, self.saver) for i in range(0, WORKER_THREADS)]
 
-                if random() <= epsilon:
-                    action_index = randrange(self.action_size)
-                else:
-                    action_index = np.argmax(q_values)
+        for agent in agents:
+            agent.start()
 
-                a_t = np.zeros([self.action_size])
-                a_t[action_index] = 1
+        for agent in agents:
+            agent.join()
 
-                if epsilon > final_epsilon:
-                    epsilon -= (initial_epsilon - final_epsilon) / anneal_epsilon_timesteps
-
-                #print("Choosing Action {}".format(action_index))
-
-                x1, x2 = action_mapper.map_action(action_index)
-                next_state, reward, term, info = env.step(x1, x2, 10)
-                next_state = np.reshape(next_state, [1, self.state_size, 1])
-                episode_reward += reward
-
-                if visualize:
-                    env.visualize()
-
-                #print("Reward: {} \n\n".format(reward))
-
-                next_q_values = self.target_model.eval(session=session, feed_dict={self.target_input: next_state})
-
-                if not term:
-                    reward = reward + gamma * np.amax(next_q_values)
-
-                if global_step % target_update_timestep == 0:
-                    session.run(self.reset_target_net)
-                    print("Target Net Resetted")
-
-                weights_old = session.run(self.network_params) ## DEBUG
-                session.run(self.update, feed_dict={self.updater_y: [reward],
-                                                    self.updater_a: [a_t],
-                                                    self.input: state})
-
-                weights_after = session.run(self.network_params) ## DEBUG
-
-                if global_step % CHECKPOINT_PERIOD_TIMESTEPS == 0:
-                    self.saver.save(session, CHECKPOINT_PATH, global_step=global_step)
-
-                global_step += 1
-                state = next_state
-                episode_step += 1
-
-                if term:
-                    # weights = session.run(self.network_params) ## DEBUG
-                    break
-
-            print("Episode {} Terminated. Rewardsum: {}, Globalstep: {}".format(num_episode, episode_reward, global_step))
+        # while num_episode <= MAX_EPISODES:
+        #     reset_env(env)
+        #     state, _, _, _ = env.step(0, 0)
+        #     state = np.reshape(state, [1, self.state_size, 1]) # @TODO Auslagern
+        #
+        #     num_episode += 1
+        #
+        #     episode_step = 0
+        #     episode_reward = 0
+        #     while True:
+        #         q_values = self.model.eval(session=session, feed_dict={self.input: state})
+        #
+        #         if random() <= epsilon:
+        #             action_index = randrange(self.action_size)
+        #         else:
+        #             action_index = np.argmax(q_values)
+        #
+        #         a_t = np.zeros([self.action_size])
+        #         a_t[action_index] = 1
+        #
+        #         if epsilon > final_epsilon:
+        #             epsilon -= (initial_epsilon - final_epsilon) / anneal_epsilon_timesteps
+        #
+        #         #print("Choosing Action {}".format(action_index))
+        #
+        #         x1, x2 = action_mapper.map_action(action_index)
+        #         next_state, reward, term, info = env.step(x1, x2, 10)
+        #         next_state = np.reshape(next_state, [1, self.state_size, 1])
+        #         episode_reward += reward
+        #
+        #         if visualize:
+        #             env.visualize()
+        #
+        #         #print("Reward: {} \n\n".format(reward))
+        #
+        #         next_q_values = self.target_model.eval(session=session, feed_dict={self.target_input: next_state})
+        #
+        #         if not term:
+        #             reward = reward + gamma * np.amax(next_q_values)
+        #
+        #         if global_step % target_update_timestep == 0:
+        #             session.run(self.reset_target_net)
+        #             print("Target Net Resetted")
+        #
+        #         weights_old = session.run(self.network_params) ## DEBUG
+        #         session.run(self.update, feed_dict={self.updater_y: [reward],
+        #                                             self.updater_a: [a_t],
+        #                                             self.input: state})
+        #
+        #         weights_after = session.run(self.network_params) ## DEBUG
+        #
+        #         if global_step % CHECKPOINT_PERIOD_TIMESTEPS == 0:
+        #             self.saver.save(session, CHECKPOINT_PATH, global_step=global_step)
+        #
+        #         global_step += 1
+        #         state = next_state
+        #         episode_step += 1
+        #
+        #         if term:
+        #             # weights = session.run(self.network_params) ## DEBUG
+        #             break
+        #
+        #     print("Episode {} Terminated. Rewardsum: {}, Globalstep: {}".format(num_episode, episode_reward, global_step))
 
     def evaluate(self, session, env):
         self.saver.restore(session, FLAGS.checkpoint_path)
@@ -179,6 +211,105 @@ class OneStepLearningAgent(object):
 
             num_episode += 1
             print('Testing Episode finished. Reward: {}'.format(episode_reward))
+
+
+class WorkerAgent(threading.Thread):
+    def __init__(self, name, graph_ops, update_ops, env, session, saver):
+        super().__init__()
+
+        self.name = name
+        self.graph_ops = graph_ops
+        self.session = session
+        self.saver = saver
+
+        self.graph_ops = graph_ops
+        self.update_ops = update_ops
+
+        self.env = Environment('test')
+        self.env.set_cluster_size(CLUSTER_SIZE)
+        self.state_size = self.env.observation_size()
+        self.action_size = action_mapper.ACTION_SIZE
+
+    def run(self):
+        global global_episode, global_step
+        print('Thread {} started.'.format(self.name))
+
+        local_episodes = 0
+        epsilon = INITIAL_EPSILON
+
+        while global_episode <= MAX_EPISODES:
+            reset_env(env)
+            state, _, _, _ = env.step(0, 0)
+            state = self.reshape_state(state)
+
+            local_episodes += 1
+            global_episode += 1
+
+            episode_step = 0
+            episode_reward = 0
+            while True:
+                q_output = self.graph_ops['network']['q_values'].eval(
+                    session=self.session,
+                    feed_dict={self.graph_ops['network']['input']: state}
+                )
+
+                if random() <= epsilon:
+                    action_index = randrange(self.action_size)
+                else:
+                    action_index = np.argmax(q_output)
+
+                a_t = np.zeros([self.action_size])
+                a_t[action_index] = 1
+
+                if epsilon > final_epsilon:
+                    epsilon -= (INITIAL_EPSILON - final_epsilon) / anneal_epsilon_timesteps
+
+                #print("Choosing Action {}".format(action_index))
+
+                x1, x2 = action_mapper.map_action(action_index)
+                next_state, reward, term, info = env.step(x1, x2, 10)
+                next_state = np.reshape(next_state, [1, self.state_size, 1])
+                episode_reward += reward
+
+                if visualize:
+                    env.visualize()
+
+                #print("Reward: {} \n\n".format(reward))
+
+                next_q_values = self.graph_ops['target_network']['q_values'].eval(
+                    session=self.session,
+                    feed_dict={self.graph_ops['target_network']['input']: next_state}
+                )
+
+                if not term:
+                    reward = reward + gamma * np.amax(next_q_values)
+
+                if global_step % target_update_timestep == 0:
+                    self.session.run(self.update_ops['reset_target_network'])
+                    print("Target Net Resetted")
+
+                #weights_old = session.run(self.network_params) ## DEBUG
+                self.session.run(self.update_ops['minimize'], feed_dict={self.update_ops['y']: [reward],
+                                                    self.update_ops['a']: [a_t],
+                                                    self.graph_ops['network']['input']: state})
+
+                #weights_after = session.run(self.network_params) ## DEBUG
+
+                if global_step % CHECKPOINT_PERIOD_TIMESTEPS == 0:
+                    self.saver.save(self.session, CHECKPOINT_PATH, global_step=global_step)
+
+                global_step += 1
+                state = next_state
+                episode_step += 1
+
+                if term:
+                    # weights = session.run(self.network_params) ## DEBUG
+                    break
+
+            print("Episode {} Terminated. Rewardsum: {}, Globalstep: {}".format(global_episode, episode_reward, global_step))
+
+    def reshape_state(self, state):
+        return np.reshape(state, [1, self.state_size, 1])
 
 
 # def train():
