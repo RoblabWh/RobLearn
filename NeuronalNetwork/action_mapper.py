4e9abc7c321a2775ca476ce587106980a9c3b0b5
Robin Marchel <hallo@rmarchel.de>
2018-09-20 18:42:57 +0200

    Change velocities

--- NeuronalNetwork/action_mapper.py
+++ NeuronalNetwork/action_mapper.py
@@ -4,19 +4,19 @@ ACTION_SIZE = 5
 def map_action(action):
     if action == 0:
         angular = 1.0
-        linear = 0.5
+        linear = 0.4
     elif action == 1:
         angular = 0.5
-        linear = 0.75
+        linear = 0.5
     elif action == 2:
         angular = 0.0
-        linear = 1.0
+        linear = 0.6
     elif action == 3:
         angular = -0.5
-        linear = 0.75
+        linear = 0.5
     elif action == 4:
         angular = -1.0
-        linear = 0.5
+        linear = 0.4
     else:
         raise AttributeError("Invalid Action: {}".format(action))
 
