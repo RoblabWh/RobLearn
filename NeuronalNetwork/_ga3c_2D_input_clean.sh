4a09bb30217389fbbd4032f229b06fe6ba711a85
ElhadjHoussem <elhadj-houssem@hotmail.com>
2018-10-22 04:28:38 +0200

    Update _ga3c_2D_input_clean.sh

--- NeuronalNetwork/_ga3c_2D_input_clean.sh
+++ NeuronalNetwork/_ga3c_2D_input_clean.sh
@@ -1,3 +1,3 @@
-rm results_2D_input.txt > /dev/null 2>&1
-rm checkpoints_2D_input_Net/* > /dev/null 2>&1
-rm logs_2D_input/*/* > /dev/null 2>&1
+rm results2D.txt > /dev/null 2>&1
+rm checkpoints2D/* > /dev/null 2>&1
+rm logs2D/*/* > /dev/null 2>&1
