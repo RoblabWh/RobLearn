4148eff1fb397d1762d782cfa2b17ec61f689ac9
ElhadjHoussem <elhadj-houssem@hotmail.com>
2018-10-22 04:27:15 +0200

    Update _ga3c_2D_input_train.sh

--- NeuronalNetwork/_ga3c_2D_input_train.sh
+++ NeuronalNetwork/_ga3c_2D_input_train.sh
@@ -1,3 +1,3 @@
-mkdir checkpoints_2D_input_Net > /dev/null 2>&1
-mkdir logs_2D_input > /dev/null 2>&1
+mkdir checkpoints2D > /dev/null 2>&1
+mkdir logs2D > /dev/null 2>&1
 python3 GA3C_2D_input.py "$@"
